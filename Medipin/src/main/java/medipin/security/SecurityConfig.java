ad9ba3ed4b1ad16513a8896ce7c7301a6f121386
jsunvelascohebert <jsun.velasco.hebert@gmail.com>
2023-08-06 18:30:23 -0400

    complete some http tests for notes, articles, and soon topics

--- Medipin/src/main/java/medipin/security/SecurityConfig.java
+++ Medipin/src/main/java/medipin/security/SecurityConfig.java
@@ -26,9 +26,33 @@ public class SecurityConfig {
         http.csrf().disable();
         http.cors();
 
-        http.authorizeRequests()
+        http.authorizeRequests() // TODO : revise these according to security needs
+
+                /* TODO ***** ***** users ***** ***** */
+
+                /* TODO ***** ***** topics ***** ***** */
+
+
+
+
+                /* ***** ***** articles ***** ***** */
+
+                .antMatchers(HttpMethod.GET, "/api/article").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/article/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/article").permitAll()
+                .antMatchers(HttpMethod.PUT, "/api/article/*").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/api/article/*").permitAll()
+
+                /* ***** ***** notes ***** ***** */
+
                 .antMatchers(HttpMethod.GET, "/api/note").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/note/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/note").permitAll()
+                .antMatchers(HttpMethod.PUT, "/api/note/*").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/api/note/*").permitAll()
+
+                /* ***** ***** misc ***** ***** */
+
                 .and()
                 .addFilter(new JwtRequestFilter(authenticationManager(authConfig),converter))
                 .sessionManagement()
