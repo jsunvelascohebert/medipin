69de9680d4a3ab00361cae0a2b4ef8dc9055cab0
jsunvelascohebert <jsun.velasco.hebert@gmail.com>
2023-09-01 10:38:36 -0400

    general backup

--- client/src/sections/general/components/Footer.js
+++ client/src/sections/general/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <section id="footer" className='z-10 mt-10'>
+    <section id="footer" className='z-10'>
       <div className="flex flex-col md:flex-row justify-between items-center p-12 bg-blue border-2 border-darkBlue gap-8">
         <h3 className="font-darkBlue">medipin</h3>
         <div className="flex flex-col md:flex-row gap-4 md:gap-8 justify-center items-center">
--- client/src/sections/general/pages/Home.js
+++ client/src/sections/general/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <section id="home">
+    <section id="home" className='bg-gradient-to-b from-lightBlue to-white'>
       {/* banner */}
       <div id="banner" className="flex flex-col w-full mx-auto py-24 md:py-32 justify-center items-center gap-5">
         <h1 className='text-center uppercase text-darkBlue'>
@@ -14,7 +14,7 @@ export default function Home() {
       </div>
       
       {/* hero image */}
-      <img id="hero-image" src="https://images.unsplash.com/photo-1576091160550-2173dba999ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" alt="temporary unsplash home image" className="object-cover w-3/4 h-80 rounded-lg mx-auto border-2 border-darkBlue shadow-md" />
+      <img id="hero-image" src="https://images.unsplash.com/photo-1576091160550-2173dba999ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" alt="temporary unsplash home image" className="object-cover w-3/4 h-80 rounded-lg mx-auto border-2 border-darkBlue shadow-md hover:-translate-y-0.5 hover:-translate-x-0.5 hover:shadow-lg transition duration-75" />
 
       {/* mission statement */}
       <div id="mission" className='flex flex-col mx-auto pt-24 px-12 max-w-xl gap-14'>
@@ -79,7 +79,7 @@ export default function Home() {
       </div>
 
       {/* disclaimer */}
-      <div id="disclaimer" className="flex flex-col justify-center items-center gap-12 p-12 max-w-xl mx-auto">
+      <div id="disclaimer" className="flex flex-col justify-center items-center gap-12 p-12 max-w-xl mx-auto mb-12">
         <h2 className="text-darkBlue">disclaimer!</h2>
         <p className='text-center'>medipin is a personal project used for practicing full-stack development skills. important to note here is the level of security that may be provided due to limited knowledge on the backend. security features and user-specific features are included; however, it is highly advised to avoid inputting any sensitive information.</p>
       </div>
